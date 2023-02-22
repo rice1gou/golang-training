338f4b80fee00935f4d4141fe785966fa16d4b02
rice1gou <rice1gou@gmail.com>
2023-02-22 20:08:06 +0900

    make dir pkg/user

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"identity/pkg/user"
 	"regexp"
 )
 
@@ -17,14 +18,14 @@ func main() {
 	flag.Parse()
 
 	re := regexp.MustCompile(`^\d{4}$`)
-	u := NewUser(*id, *age, *name, *email)
+	u := user.NewUser(*id, *age, *name, *email)
 
 	if err := initUser(re, u); err != nil {
 		fmt.Println(err.Error())
 	}
 }
 
-func initUser(re *regexp.Regexp, u *User) error {
+func initUser(re *regexp.Regexp, u *user.User) error {
 	if match := re.MatchString(u.ID); !match {
 		return fmt.Errorf("invalid identity")
 	}
