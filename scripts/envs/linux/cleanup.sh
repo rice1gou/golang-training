c4ea3f44c4437a34468f8011196f964e3fe97f3e
rice1gou <rice1gou@gmail.com>
2023-03-03 22:00:04 +0900

    modified scriptfiles

--- scripts/envs/linux/cleanup.sh
+++ scripts/envs/linux/cleanup.sh
@@ -2,5 +2,5 @@
 SCRIPT_DIR=$(dirname "$0")
 cd "$SCRIPT_DIR"
 
-cd ../../build
+cd ../../../build
 docker-compose down --rmi all --volumes --remove-orphans
--- scripts/envs/linux/start.sh
+++ scripts/envs/linux/start.sh
@@ -2,5 +2,5 @@
 SCRIPT_DIR=$(dirname "$0")
 cd "$SCRIPT_DIR"
 
-cd ../../build
+cd ../../../build
 docker-compose up -d
