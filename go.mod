2c30a401bfedab409c5b2cb596013e4d70ea0653
rice1gou <rice1gou@gmail.com>
2023-03-03 20:43:37 +0900

    modifi handler

--- go.mod
+++ go.mod
@@ -4,4 +4,4 @@ go 1.18
 
 require github.com/lib/pq v1.10.7
 
-require github.com/google/uuid v1.3.0 // indirect
+require github.com/google/uuid v1.3.0
--- handler/handler.go
+++ handler/handler.go
@@ -1,41 +1,67 @@
 package handler
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
 )
 
-func IndexHandler(w http.ResponseWriter, r *http.Request) {
+func IndexHandler(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "top page\n")
+	}
 }
-func SigninHandler(w http.ResponseWriter, r *http.Request) {
+
+func SigninHandler(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 	// サインイン処理
 	fmt.Fprint(w, "signin\n")
+	}
 }
-func SignoutHandler(w http.ResponseWriter, r *http.Request) {
+
+func SignoutHandler(db *sql.DB) http.HandlerFunc{
+	return func(w http.ResponseWriter, r *http.Request) {
 	// サインアウト処理
 	fmt.Fprint(w, "signout\n")
+	}
 }
-func SignupHandler(w http.ResponseWriter, r *http.Request) {
+
+func SignupHandler(db *sql.DB) http.HandlerFunc{
+	return func(w http.ResponseWriter, r *http.Request) {
 	// サインアップ処理
 	fmt.Fprint(w, "signup\n")
+	}
 }
-func FetchUsersHandler(w http.ResponseWriter, r *http.Request) {
+func FetchUsersHandler(db *sql.DB) http.HandlerFunc{
+	return func(w http.ResponseWriter, r *http.Request) {
 	// ユーザー一覧取得
 	fmt.Fprint(w, "get users list\n")
+	}
 }
-func FetchUserHandler(w http.ResponseWriter, r *http.Request) {
+
+func FetchUserHandler(db *sql.DB) http.HandlerFunc{
+	return func(w http.ResponseWriter, r *http.Request) {
 	// ユーザー詳細取得
 	fmt.Fprint(w, "get user details\n")
+	}
 }
-func SaveUserHandler(w http.ResponseWriter, r *http.Request) {
+
+func SaveUserHandler(db *sql.DB) http.HandlerFunc{
+	return func(w http.ResponseWriter, r *http.Request) {
 	// ユーザー登録
 	fmt.Fprint(w, "register user\n")
+	}
 }
-func ModifyUserHandler(w http.ResponseWriter, r *http.Request) {
+
+func ModifyUserHandler(db *sql.DB) http.HandlerFunc{
+	return func(w http.ResponseWriter, r *http.Request) {
 	// ユーザー編集
 	fmt.Fprint(w, "modify user\n")
+	}
 }
-func DeleteUserHandler(w http.ResponseWriter, r *http.Request) {
+
+func DeleteUserHandler(db *sql.DB) http.HandlerFunc{
+	return func(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "delete user\n")
+	}
 }
