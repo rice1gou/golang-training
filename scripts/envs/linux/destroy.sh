fe4dde9064b6681bcb01193ebdf521d851c6cdd7
rice1gou <rice1gou@gmail.com>
2023-04-10 14:52:06 +0900

    modified directory struct

--- scripts/envs/linux/destroy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-SCRIPT_DIR=$(dirname "$0")
-cd "$SCRIPT_DIR"
-
-cd ../../../build
-docker-compose down --volumes --remove-orphans
--- scripts/envs/linux/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-SCRIPT_DIR=$(dirname "$0")
-cd "$SCRIPT_DIR"
-
-cd ../../../build
-docker-compose up -d
